58be2a435bf7a7f4dcf782d0f81c833ce0cd4e60
Gleb Liutsko <gleb290303@gmail.com>
2020-02-05 16:03:08 +0400

    Изменена структура проекта
    Добавлен __init__ в пакет vk_bot
    Файл utils/types.py переименован в utils.py

--- vk_bot/keyboard/buttons/__init__.py
+++
@@ -1,5 +0,0 @@
-from .button import Button
-from .button_text import ButtonText
-from .button_location import ButtonLocation
-from .button_vk_pay import ButtonVKPay
-from .button_vk_apps import ButtonVKApp
