ff3c1f9bb193eb11a749e25bec97300ec37c4f92
Gleb Liutsko <gleb290303@gmail.com>
2020-05-11 16:33:57 +0400

    Перевод на библиотеку vkbottle

--- utils.py
+++ utils.py
@@ -1,23 +1,9 @@
-def to_list(val):
-    if val is None:
-        return []
+import os
+import random
+import json
 
-    if isinstance(val, list):
-        return val
-
-    return [val]
-
-
-def string_list_lower(list_str: list):
-    return [i.lower() for i in list_str]
-
-
-def bool2int(val: bool):
-    return int(val) if val is not None else None
-
-
-def get_user(session):
-    pass
+from vkbottle.types.responses.photos import GetMessagesUploadServer
+import aiohttp
 
 
 def number_weekday_to_text(number):
@@ -35,3 +21,35 @@ def number_weekday_to_text(number):
         return 'Сб'
     elif number == 6:
         return 'Вс'
+
+
+def get_random():
+    return random.getrandbits(31) * random.choice([-1, 1])
+
+
+async def upload_photo(upload_server: GetMessagesUploadServer, path):
+    print('GHafjsdfksdfkjshdfkjshdfksdhfksjfhskdjfh------------------------------')
+    url = upload_server.upload_url
+
+    file_extension = os.path.splitext(path)[1]
+    if file_extension == '.png':
+        content_type = 'image/png'
+    elif file_extension in ('.jpg', '.jpeg'):
+        content_type = 'image/jpeg'
+    elif file_extension == '.gif':
+        content_type = 'image/gif'
+    else:
+        raise Exception()
+
+    data = aiohttp.FormData()
+    data.add_field('photo',
+                   open(path, 'rb'),
+                   filename=os.path.split(path)[1],
+                   content_type=content_type)
+
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, data=data) as resp:
+            data_photo_upload = await resp.text()
+            data_photo_upload = json.loads(data_photo_upload)
+
+    return data_photo_upload['server'], data_photo_upload['photo'], data_photo_upload['hash']
