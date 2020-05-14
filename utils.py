191c765dd202304fa862bfe9e993027872b69820
Gleb Liutsko <gleb290303@gmail.com>
2020-05-14 10:32:14 +0400

    Рефакторинг функции utils.upload_photo

--- utils.py
+++ utils.py
@@ -1,8 +1,7 @@
 import os
 import random
 import json
-from typing import Tuple
-
+from typing import Tuple, NamedTuple
 from vkbottle.types.responses.photos import GetMessagesUploadServer
 import aiohttp
 
@@ -28,31 +27,41 @@ def get_random() -> int:
     return random.getrandbits(31) * random.choice([-1, 1])
 
 
-async def upload_photo(upload_server: GetMessagesUploadServer, path) -> Tuple[str, str, str]:
-    url = upload_server.upload_url
+class PhotoInfo(NamedTuple):
+    server: str
+    photo: str
+    hash: str
 
-    file_extension = os.path.splitext(path)[1]
-    if file_extension == '.png':
-        content_type = 'image/png'
-    elif file_extension in ('.jpg', '.jpeg'):
-        content_type = 'image/jpeg'
-    elif file_extension == '.gif':
-        content_type = 'image/gif'
-    else:
-        raise Exception()
+
+async def upload_photo(upload_server: GetMessagesUploadServer, path: str) -> PhotoInfo:
+    if not os.path.isfile(path):
+        raise FileNotFoundError
+
+    def get_content_type():
+        file_extension = os.path.splitext(path)[1]
+        if file_extension == '.png':
+            return 'image/png'
+        elif file_extension in ('.jpg', '.jpeg'):
+            return 'image/jpeg'
+        elif file_extension == '.gif':
+            return 'image/gif'
+        else:
+            raise Exception()
+
+    url = upload_server.upload_url
 
     data = aiohttp.FormData()
     data.add_field('photo',
                    open(path, 'rb'),
                    filename=os.path.split(path)[1],
-                   content_type=content_type)
+                   content_type=get_content_type())
 
     async with aiohttp.ClientSession() as session:
         async with session.post(url, data=data) as resp:
             data_photo_upload = await resp.text()
             data_photo_upload = json.loads(data_photo_upload)
 
-    return data_photo_upload['server'], data_photo_upload['photo'], data_photo_upload['hash']
+    return PhotoInfo(**data_photo_upload)
 
 
 def parse_massive_int_env(name: str, sep: str = ',') -> Tuple[int]:
