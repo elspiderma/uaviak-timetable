e03a821883513140645601d824015ff728dec957
Gleb Liutsko <gleb290303@gmail.com>
2020-05-14 09:39:14 +0400

    Удалены ненужные настройки

--- config.py
+++ config.py
@@ -1,9 +1,6 @@
 import os
 
 TOKEN_BOT = os.getenv('TOKEN_BOT')
-CONFIRMATION_TOKEN = os.getenv('CONFIRMATION_TOKEN')
-SECRET = os.getenv('SECRET')
-
 DATA_BASE = os.getenv('DATA_BASE')
 
 STATIC_DIR = os.getenv('STATIC_DIR', f'{os.path.dirname(__file__)}/static')
