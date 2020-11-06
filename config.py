af7ee7a0f3076f2eba194fecd93de21c19fcf987
Gleb Liutsko <gleb290303@gmail.com>
2020-11-06 17:49:46 +0400

    Cache timetable in DB

--- config.py
+++ config.py
@@ -7,3 +7,7 @@ DATA_BASE = os.getenv('DATA_BASE')
 
 STATIC_DIR = os.getenv('STATIC_DIR', f'{os.path.dirname(__file__)}/static')
 ADMIN_ID = parse_massive_int_env('ADMIN_ID')
+
+TMPDIR = os.getenv('TMPDIR', '/tmp')
+if not os.path.isdir(TMPDIR):
+    os.mkdir(TMPDIR)
--- update_timetable.py
+++ update_timetable.py
@@ -1,31 +1,67 @@
 import asyncio
+import typing
 
-from utils.timetablecache import TimetableCache
 import db
+from timetable.timetable_async import TimetableCache
 
 
-async def update_timetable() -> bool:
-    timetable = await TimetableCache.load_from_site_if_updated()
-    if timetable is None:
-        return False
+async def update_timetable():
+    modified = {
+        'lessons': set(),
+        'teachers': set(),
+        'groups': set()
+    }
 
-    db.session.query(db.TimetableDB).delete()
+    timetable = await TimetableCache.load()
+    if not timetable:
+        return modified
 
     for lesson in timetable:
-        db.session.add(db.TimetableDB(
+        group = await db.Group.filter(title=lesson.group).first()
+        if group is None:
+            group = await db.Group.create(title=lesson.group)
+
+        teacher = await db.Teacher.filter(short_name=lesson.teacher).first()
+        if teacher is None:
+            teacher = await db.Teacher.create(short_name=lesson.teacher)
+
+        is_exists_lesson = await db.Timetable.filter(
             date=timetable.date,
-            group=lesson.group,
+            group=group,
             number=lesson.number,
             cabinet=lesson.cabinet,
-            teacher=lesson.teacher,
+            teacher=teacher,
             subject=lesson.subject,
             is_splitting=lesson.is_splitting,
             is_practice=lesson.is_practice,
             is_consultations=lesson.is_consultations,
             is_exam=lesson.is_exam
-        ))
+        ).exists()
+        if not is_exists_lesson:
+            lesson = await db.Timetable.create(
+                date=timetable.date,
+                group=group,
+                number=lesson.number,
+                cabinet=lesson.cabinet,
+                teacher=teacher,
+                subject=lesson.subject,
+                is_splitting=lesson.is_splitting,
+                is_practice=lesson.is_practice,
+                is_consultations=lesson.is_consultations,
+                is_exam=lesson.is_exam
+            )
+            modified['lessons'].add(lesson)
+            modified['teachers'].add(teacher)
+            modified['groups'].add(group)
 
-    db.session.commit()
+    return modified
+
+
+async def main():
+    await db.init()
+    print(await update_timetable())
+    await db.stop()
+
 
 if __name__ == '__main__':
-    print(asyncio.run(update_timetable()))
+    asyncio.run(main())
