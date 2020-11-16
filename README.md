82ff0b52b1f99817685b00d57da631006a19958b
Gleb Liutsko <gleb290303@gmail.com>
2020-11-16 15:40:28 +0400

    Переменная окружения DEBUG_ENABLE, для включения режима отладки

--- README.md
+++ README.md
@@ -4,8 +4,10 @@
  - `TOKEN_BOT` - VK API token
  - `DATA_BASE` - URL бызы данных (например `sqlite:///db.sqlite`, `postgresql://username:password@localhost/dbname`)
 )
- - `STATIC_DIR` - (optional) директория со статичными файлами
  - `ADMIN_ID` - ID пользователя-администратора
+ - `DEBUG_ENABLE` - (optional) Если 1, то включает режим отладки.
+ - `STATIC_DIR` - (optional) директория со статичными файлами
+ - `TMPDIR` - (optional) директория с временными файлами
  
 ## Команды доступные только администратору
  - `upd` - рассылает расписание пользователям
--- timetable_bot/config.py
+++ timetable_bot/config.py
@@ -1,19 +1,23 @@
+import locale
+import logging
 import os
 
 from utils.env import parse_massive_int_env
-import locale
 
 TOKEN_BOT = os.getenv('TOKEN_BOT')
 DATA_BASE = os.getenv('DATA_BASE')
 
 STATIC_DIR = os.getenv('STATIC_DIR', f'{os.path.dirname(__file__)}/static')
-ADMIN_ID = parse_massive_int_env('ADMIN_ID')
-
-locale.setlocale(locale.LC_ALL, os.getenv('LANG'))
-
 TMPDIR = os.getenv('TMPDIR', '/tmp')
 if not os.path.isdir(TMPDIR):
     os.mkdir(TMPDIR)
+
+ADMIN_ID = parse_massive_int_env('ADMIN_ID')
+
+locale.setlocale(locale.LC_ALL, os.getenv('LANG'))
+DEBUG = bool(int(os.getenv('DEBUG_ENABLE', '0')))
+if DEBUG:
+    logging.basicConfig(level=logging.DEBUG)
 
 TORTOISE_ORM = {
     "connections": {"default": DATA_BASE},
