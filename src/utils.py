2bc9b10ed63d7c7ce8bc24f00a48874d44b99323
Gleb Liutsko <gleb@liutsko.ru>
2021-07-12 20:40:25 +0400

    Изменение ask_yes_no
    Добавлена документация к функции
    Ответ не чусвителен к регистру

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,13 @@
 def ask_yes_no(text: str, default: bool = False) -> bool:
+    """Спрашивает в консоле у пользователя 'Yes' или 'No'.
+
+    Args:
+        text: Текст, который будет выведен в приведствии.
+        default: Значение по умолчанию.
+
+    Returns:
+        True - если пользователь ответил 'Yes', False - если пользователь ответил 'No'.
+    """
     yes_value = ('y', 'yes', '1')
     no_value = ('n', 'no', '0')
 
@@ -9,10 +18,10 @@ def ask_yes_no(text: str, default: bool = False) -> bool:
     else:
         value_prompt[1] = 'N'
 
-    prompt = f'{text} ({value_prompt[0]}/{value_prompt[1]}) '
+    prompt = f'{text} ({value_prompt[0]}/{value_prompt[1]}): '
 
     while True:
-        s = input(prompt)
+        s = input(prompt).lower()
 
         if s in yes_value:
             return True
