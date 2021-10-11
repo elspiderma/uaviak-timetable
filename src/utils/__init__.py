14d76c45c78bed94a91605c5f57029810d461a0f
Gleb Liutsko <gleb@liutsko.ru>
2021-10-11 17:30:44 +0400

    Документация для DrawnTable

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,2 +1,3 @@
 from .stdin import ask_yes_no
+from .drawn_table import DrawnTable, Cell
 from .string_utils import is_string_one_unique_char, index_upper
--- src/utils/drawn_table.py
+++ src/utils/drawn_table.py
@@ -5,14 +5,24 @@ from PIL import ImageFont, Image, ImageDraw
 
 @dataclass
 class Cell:
+    """Ячейка таблицы.
+    """
     text: str
     color: tuple[int, int, int] = (0, 0, 0)
     background_color: tuple[int, int, int] = (255, 255, 255)
 
 
-class DrawerTable:
+class TableDrawError(Exception):
+    """Ошибка рисования таблицы.
+    """
+    pass
+
+
+class DrawnTable:
+    """Класс, представляющий нарисованную таблицу.
+    """
     def __init__(self, font: str, size_font: int, padding_horizontal: int = 10, padding_vertical: int = 2,
-                 line_thickness: int = 1):
+                 line_thickness: int = 3):
         self._cells: list[list[Cell]] = [[]]
 
         self._font = ImageFont.truetype(font, size_font)
@@ -21,7 +31,13 @@ class DrawerTable:
         self.padding_vertical = padding_vertical
         self.line_thickness = line_thickness
 
-    def append(self, cell: Cell, row: int = None):
+    def append(self, cell: Cell, row: int = None) -> None:
+        """Добавляет новую ячейку в ряд.
+
+        Args:
+            cell: Ячейка.
+            row: Номер ряда, если None, то используется самый последний.
+        """
         if row is None:
             row = -1
         elif row < 0 or row >= len(self._cells):
@@ -29,10 +45,33 @@ class DrawerTable:
 
         self._cells[row].append(cell)
 
-    def row(self):
+    def row(self) -> None:
+        """Добавляет новый пустой ряд.
+        """
         self._cells.append(list())
 
+    def is_correct_table(self) -> bool:
+        """Проверяет, является ли таблица корректной.
+
+        Returns:
+            True -- если таблица корректна, иначе False.
+        """
+        # Считаем количество ячеек в каждом ряде.
+        count_cells_in_row = [len(i) for i in self._cells]
+
+        conditions = (
+            len(set(count_cells_in_row)) == 1,  # Все строки должны содержать одинаковое кол-во ящеек.
+            0 not in count_cells_in_row  # Ряд в таблице должен содержать хотя бы одну ячейку.
+        )
+
+        return all(conditions)
+
     def _get_size_columns_and_row(self) -> tuple[list[int], list[int]]:
+        """Возвращает длины столбцов и высоты строк.
+
+        Returns:
+            Кортеж, в котором нулевой элемент -- длины столбцов, а первый элемент -- высоты строк.
+        """
         columns = list(zip(*self._cells))
 
         wight_text_columns = [0 for i in range(len(columns))]
@@ -53,6 +92,15 @@ class DrawerTable:
         return wight_columns, height_rows
 
     def _get_size_table(self) -> tuple[list[int], list[int], int, int]:
+        """Возвращает длины столбцов, высоты строк и общие размеры таблицы.
+
+        Returns:
+            Кортеж, в котором
+                нулевой элемент -- длины столбцов,
+                первый элемент -- высоты строк,
+                второй элемент -- ширина таблицы,
+                третий элемент -- высота таблицы
+        """
         def calculate_total_size(sizes: list[int]) -> int:
             return sum(sizes) + (len(sizes) - 1) * self.line_thickness
 
@@ -64,15 +112,22 @@ class DrawerTable:
         return wight_columns, height_rows, table_width, table_height
 
     def draw(self) -> Image:
+        """Рисует таблицу.
+
+        Returns:
+            Изображение с таблицой.
+        """
+        if not self.is_correct_table():
+            raise TableDrawError('This table is invalid')
+
         wight_columns, height_rows, table_width, table_height = self._get_size_table()
 
         img = Image.new('RGB', (table_width, table_height), (255, 255, 255))
         img_draw = ImageDraw.Draw(img)
 
-        x = 0
-        y = 0
-        for n_row, row in enumerate(self._cells):
-            for n_column, cell in enumerate(row):
+        x, y = 0, 0
+        for n_row, (height_row, row) in enumerate(zip(height_rows, self._cells)):
+            for n_column, (wight_column, cell) in enumerate(zip(wight_columns, row)):
                 # Линии отделяющие колонки рисуем только один раз, при прорисовке первого ряда (n_row == 0),
                 # а так же не рисуем их для 1 колонки.
                 if n_column != 0 and n_row == 0:
@@ -84,12 +139,12 @@ class DrawerTable:
 
                 # Задний фон ячейки.
                 img_draw.rectangle(
-                    xy=((x, y), (x + wight_columns[n_column], y + height_rows[n_row])),
+                    xy=((x, y), (x + wight_column, y + height_row)),
                     fill=cell.background_color
                 )
 
                 # Рисуем текст по середине ячейки.
-                text_x = x + wight_columns[n_column] / 2 - self._font.getsize(cell.text)[0] / 2
+                text_x = x + wight_column / 2 - self._font.getsize(cell.text)[0] / 2
                 text_y = y + self.padding_vertical
 
                 img_draw.text(
@@ -99,7 +154,7 @@ class DrawerTable:
                     fill=cell.color
                 )
 
-                x += wight_columns[n_column] + self.line_thickness
+                x += wight_column + self.line_thickness
 
             # Рисуем разделитель строк начиная со 2 строки.
             if n_row != 0:
@@ -110,6 +165,6 @@ class DrawerTable:
                 )
 
             x = 0
-            y += height_rows[n_row] + self.line_thickness
+            y += height_row + self.line_thickness
 
         return img
